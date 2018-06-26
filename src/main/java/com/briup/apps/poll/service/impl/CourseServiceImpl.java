caf32c0ef4c3ac8933d42252bf53d6f8ccea122c
Nase <Nase@Nase>
2018-06-26 16:32:40 +0800

    学校相关接口。 用户相关接口。

--- src/main/java/com/briup/apps/poll/service/impl/CourseServiceImpl.java
+++ src/main/java/com/briup/apps/poll/service/impl/CourseServiceImpl.java
@@ -17,18 +17,21 @@ public class CourseServiceImpl implements ICourseService{
 	private CourseMapper courseMapper;
 
 	@Override
+	//查询所有的课程信息
 	public List<Course> findAll() throws Exception {
 		CourseExample example = new CourseExample();
 		return courseMapper.selectByExample(example);
 	}
 
 	@Override
+	//通过ID查询课程信息
 	public Course findById(long id) throws Exception {
 		// TODO Auto-generated method stub
 		return courseMapper.selectByPrimaryKey(id);
 	}
 
 	@Override
+	//通过关键字查询课程信息
 	public List<Course> query(String Keywords) throws Exception {
 		CourseExample example = new CourseExample();
 		//模板查询 添加了一个文件，name 属性包含Keywords关键字    可以用andDescription();
@@ -37,6 +40,7 @@ public class CourseServiceImpl implements ICourseService{
 	}
 
 	@Override
+	//ID不为空时更新课程信息  ID为空时保存课程信息
 	public void saveOrupdate(Course course) throws Exception {
 		if(course.getId()!=null) {
 			//更新
@@ -49,12 +53,14 @@ public class CourseServiceImpl implements ICourseService{
 	}
 
 	@Override
+	//通过ID删除课程信息
 	public void deleteById(long id) throws Exception {
 		// TODO Auto-generated method stub
 		courseMapper.deleteByPrimaryKey(id);
 	}
 
 	@Override
+	//批量删除课程信息
 	public void batchDelete(List<Long> ids) throws Exception {
 		//遍历ids中的id
 		for(long id:ids) {
